b331f63178750e9f3683424ec10222c619c2323a
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-03-06 18:26:34 +0300

    feat: add header to app.tsx

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -2,14 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/main/main";
 import About from "./pages/about/about";
 import Page404 from "./pages/404/404";
+import Header from "./containers/header/header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<Page404 />} />
-    </Routes>
+    <>
+      <Header />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
