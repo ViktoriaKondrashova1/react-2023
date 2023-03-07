61994d3dfe6579262f706aeb124a6b5f156d052d
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-03-07 14:13:11 +0300

    feat: add 404 page styles

--- react-components/src/pages/404/404.tsx
+++ react-components/src/pages/404/404.tsx
@@ -5,8 +5,8 @@ function Page404() {
   return (
     <div className="page404">
       <div className="container">
-        <h1>404</h1>
-        <h2>The page was not found</h2>
+        <h1 className="page404__title">404</h1>
+        <h2 className="page404__subtitle">The page was not found</h2>
         <Link to="/">Go to the main page</Link>
       </div>
     </div>
