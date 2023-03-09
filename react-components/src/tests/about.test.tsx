dd7ec03b512a4e3099ea5677d63a39ab5ec751cb
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-03-09 16:48:58 +0300

    feat: add about page test

---
+++ react-components/src/tests/about.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it } from "vitest";
+import About from "../pages/about/about";
+
+describe("About", () => {
+  it("Renders the about-us page", () => {
+    render(<About />);
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      "About Us"
+    );
+  });
+});
