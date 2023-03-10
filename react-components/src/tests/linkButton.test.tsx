bef096b539521885eb9ef5967a29c94315798a91
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-03-10 18:12:46 +0300

    feat: add linkButton component tests

---
+++ react-components/src/tests/linkButton.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import LinkButton from "../components/linkButton/linkButton";
+
+describe("LinkButton component", () => {
+  it("Renders linkButton without text", () => {
+    render(
+      <BrowserRouter>
+        <LinkButton link="/" />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toHaveAttribute("type", "button");
+    expect(screen.getByRole("link")).toBeInTheDocument();
+    expect(screen.getByRole("link")).toBeEnabled();
+  });
+
+  it("Renders linkButton with text", () => {
+    render(
+      <BrowserRouter>
+        <LinkButton link="/" text="Click me" />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("Click me")).toBeInTheDocument();
+  });
+});
