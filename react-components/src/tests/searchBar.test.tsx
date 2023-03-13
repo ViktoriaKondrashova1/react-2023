b41ffa3d426f38ca4890874d6e962566e4635cc5
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-03-13 18:23:33 +0300

    feat: add onChange test for searchbox

--- react-components/src/tests/searchBar.test.tsx
+++ react-components/src/tests/searchBar.test.tsx
@@ -1,5 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { vi } from "vitest";
 import SearchBar from "../components/searchBar/searchBar";
+
+const handleChange = vi.fn();
 
 describe("SearchBar component", () => {
   it("Renders search bar", () => {
@@ -10,4 +13,14 @@ describe("SearchBar component", () => {
     expect(screen.getByPlaceholderText("Search...")).toBeInTheDocument();
     expect(screen.getByRole("searchbox")).toHaveAttribute("type", "search");
   });
+
+  it("onChange works"),
+    () => {
+      render(<SearchBar value="" handleChange={handleChange} />);
+
+      fireEvent.change(screen.getByRole("textbox"), "test");
+
+      expect(handleChange).toHaveBeenCalled();
+      expect(handleChange).toHaveBeenCalledTimes(4);
+    };
 });
