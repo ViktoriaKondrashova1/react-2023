38d8eefcf59c42f4b5698c3b741fc6bde9a364b6
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-03-15 16:40:53 +0300

    feat: add header title display

--- react-components/src/containers/header/header.tsx
+++ react-components/src/containers/header/header.tsx
@@ -2,12 +2,33 @@ import React from "react";
 import Navigation from "../../components/navigation/navigation";
 import "./header.scss";
 
-class Header extends React.Component {
+interface HeaderState {
+  title: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+class Header extends React.Component<{}, HeaderState> {
+  constructor(props: React.Component) {
+    super(props);
+    this.state = {
+      title: "",
+    };
+  }
+  updateTitle = () => {
+    const pages: object = {
+      "/": "MAIN PAGE",
+      "/about": "ABOUT PAGE",
+    };
+    const titleName = pages[location.pathname as keyof object] || "";
+    this.setState({ title: titleName });
+  };
   render() {
+    this.updateTitle();
     return (
       <header className="header">
-        <div className="container">
+        <div className="container header__container">
           <Navigation />
+          <h1 className="header__title">{this.state.title}</h1>
         </div>
       </header>
     );
