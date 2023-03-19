876633b87538f46711eb0b3a2f261c259830d9ec
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-03-19 15:05:20 +0300

    refactor: add base input

---
+++ react-components/src/components/inputs/baseInput/baseInput.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./baseInput.scss";
+
+interface baseInputProps {
+  name: string;
+  type: string;
+}
+
+class BaseInput extends React.Component<baseInputProps> {
+  constructor(props: baseInputProps) {
+    super(props);
+  }
+  render() {
+    return (
+      <form className="base-input">
+        <label htmlFor={this.props.name} className="base-input__label">
+          {this.props.name
+            .split("-")
+            .map((word) => word[0].toUpperCase() + word.substring(1))
+            .join(" ")}
+          <br />
+          <input
+            type={this.props.type}
+            name={this.props.name}
+            className="base-input__input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default BaseInput;
--- react-components/src/containers/form/form.tsx
+++ react-components/src/containers/form/form.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import TextInput from "../../components/inputs/textInput/textInput";
+import BaseInput from "../../components/inputs/baseInput/baseInput";
 import "./form.scss";
 
 class Form extends React.Component {
   render() {
     return (
       <div className="form">
-        <div className="form__name">
-          <TextInput name="first-name" />
-          <TextInput name="last-name" />
+        <div className="form__wrapper">
+          <BaseInput name="first-name" type={"text"} />
+          <BaseInput name="last-name" type={"text"} />
+        </div>
+        <div className="form__wrapper">
+          <BaseInput name="date-of-birth" type={"date"} />
         </div>
       </div>
     );
