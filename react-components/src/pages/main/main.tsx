a5cd362383f90077709d06ebd404cdedf45131be
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-04-06 17:50:06 +0300

    feat: add progressing

--- react-components/src/pages/main/main.tsx
+++ react-components/src/pages/main/main.tsx
@@ -9,13 +9,18 @@ const url = "https://rickandmortyapi.com/api/character";
 const Main = () => {
   const [searchValue, setSearchValue] = useState<string>();
   const [searchResult, setSearchResult] = useState<CharacterProps[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    fetch(searchValue ? `${url}?name=${searchValue}` : url)
-      .then((res) => res.json())
-      .then((data) => {
-        setSearchResult(data.results);
-      });
+    setTimeout(() => {
+      fetch(searchValue ? `${url}?name=${searchValue}` : url)
+        .then((res) => res.json())
+        .then((data) => {
+          setSearchResult(data.results);
+          setIsLoading(false);
+        })
+        .catch((error) => console.log(error));
+    }, 500);
   }, [searchValue]);
 
   const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -28,6 +33,7 @@ const Main = () => {
     <div className="main">
       <div className="container">
         <SearchBar handleKeyDown={handleEnter} />
+        {isLoading && <div className="main__loading">Progressing...</div>}
         <CardList data={searchResult} />
       </div>
     </div>
