83036e11ae1bdddd176ef91f103e5f1b5d1973a8
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-04-29 13:08:06 +0300

    fix: fix app tsx

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,17 +1,14 @@
 import Router from "./router";
-import Html from "./html";
 import Header from "./containers/header/header";
 
 const App = () => {
   return (
-    <Html>
-      <>
-        <Header />
-        <main>
-          <Router />
-        </main>
-      </>
-    </Html>
+    <>
+      <Header />
+      <main>
+        <Router />
+      </main>
+    </>
   );
 };
 
--- react-components/src/entry-server.tsx
+++ react-components/src/entry-server.tsx
@@ -6,7 +6,6 @@ import { StaticRouter } from "react-router-dom/server";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store/store";
-// import { Response } from "express";
 
 export function render(url: string, opts: RenderToPipeableStreamOptions) {
   const stream = renderToPipeableStream(
@@ -19,24 +18,3 @@ export function render(url: string, opts: RenderToPipeableStreamOptions) {
   );
   return stream;
 }
-
-// export function render(
-//   url: string,
-//   opts: RenderToPipeableStreamOptions,
-//   res: Response
-// ) {
-//   const stream = renderToPipeableStream(
-//     <Provider store={store}>
-//       <StaticRouter location={url}>
-//         <App />
-//       </StaticRouter>
-//     </Provider>,
-//     {
-//       bootstrapScripts: ["/main.js"],
-//       onShellReady() {
-//         res.setHeader("content-type", "text/html");
-//         stream.pipe(res);
-//       },
-//     }
-//   );
-// }
